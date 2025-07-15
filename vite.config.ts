ec5a742ce430078b46bbccd9dcebb09eb7255179
Erez Schlosser <erezschlosser@users.noreply.github.com>
2025-07-15 18:41:19 +0200

    a

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/real-estate-dashboard/', // change to your repo name
-  plugins: [react()],
+  base: '/real-estate-dashboard/', // same as before
+  build: {
+    outDir: 'docs',
+  },
 });
